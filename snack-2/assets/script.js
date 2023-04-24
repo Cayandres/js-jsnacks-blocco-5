3bf155c62dc97fcb94bb6e7f120ee64f17d5af68
Andres Cayambe <andrescayambe99@hotmail.com>
2023-04-24 13:07:55 +0200

    [add-average-vote-and-end-snack2]

--- snack-2/assets/script.js
+++ snack-2/assets/script.js
@@ -10,62 +10,149 @@ const users = [
     firstname: 'Andres',
     lastname:  'Cayambe',
     idcode:    getRandomNumber(9999,100000),
-    votes: {
-      spanish: getRandomNumber(1,10),
-      english: getRandomNumber(1,10),
-      history: getRandomNumber(1,10),
-      math:    getRandomNumber(1,10)
-    }
-     
+    votes: [
+      {
+        subject: 'spanish',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'english',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'history',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'math',
+        vote: getRandomNumber(1,10)
+      }
+  
+    ]
   },
   {
     firstname: 'Stefania',
     lastname:  'Scandaglini',
     idcode:    getRandomNumber(9999,100000),
-    votes: {
-      spanish: getRandomNumber(1,10),
-      english: getRandomNumber(1,10),
-      history: getRandomNumber(1,10),
-      math:    getRandomNumber(1,10)
-    }
+    votes: [
+      {
+        subject: 'spanish',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'english',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'history',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'math',
+        vote: getRandomNumber(1,10)
+      }
+  
+    ]
   },
   {
     firstname: 'Jacqueline',
     lastname:  'Brogi',
     idcode:    getRandomNumber(9999,100000),
-    votes: {
-      spanish: getRandomNumber(1,10),
-      english: getRandomNumber(1,10),
-      history: getRandomNumber(1,10),
-      math:    getRandomNumber(1,10)
-    }
+    votes: [
+      {
+        subject: 'spanish',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'english',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'history',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'math',
+        vote: getRandomNumber(1,10)
+      }
+  
+    ]
   },
   {
     firstname: 'Rosa',
     lastname:  'Pilicita',
     idcode:    getRandomNumber(9999,100000),
-    votes: {
-      spanish: getRandomNumber(1,10),
-      english: getRandomNumber(1,10),
-      history: getRandomNumber(1,10),
-      math:    getRandomNumber(1,10)
-    }
+    votes: [
+      {
+        subject: 'spanish',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'english',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'history',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'math',
+        vote: getRandomNumber(1,10)
+      }
+  
+    ]
   },
   {
     firstname: 'Ramon',
     lastname:  'Ulcuango ',
     idcode:    getRandomNumber(9999,100000), 
-    votes: {
-      spanish: getRandomNumber(1,10),
-      english: getRandomNumber(1,10),
-      history: getRandomNumber(1,10),
-      math:    getRandomNumber(1,10)
-    }
+    votes: [
+      {
+        subject: 'spanish',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'english',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'history',
+        vote: getRandomNumber(1,10)
+      },
+      {
+        subject: 'math',
+        vote: getRandomNumber(1,10)
+      }
+  
+    ]
   }
 ]
+
+const usersAverage = users.map (user =>{
+
+  const { firstname , lastname , idcode, votes } = user; 
+  console.log(averageVotes(votes));
+  const userAverage = {
+    firstname: `${firstname}`,
+    lastname : `${lastname}`,
+    idcode   : `${idcode}`,
+    votes    : `${averageVotes(votes)}`,
+ 
+  }
+   return userAverage;
+})
+
+function averageVotes(votes) {
+  let sum = 0;
+  votes.forEach(exam => {
+    sum += exam.vote
+  });
+  return Math.round( sum / votes.length );
+}
+
 
 function getRandomNumber(min,max) {
  return  Math.floor( Math.random() * ( max - min + 1 ) + min )
 }   
 
-console.log(users);
+console.log(usersAverage);
